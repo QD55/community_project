934f37e4a9fb9a378c998086e0899088a1683e10
QD55 <630197249@qq.com>
2023-03-09 17:15:50 +0800

    退出登录功能实现

--- src/main/java/com/yau/doubao_community/controller/UmsUserController.java
+++ src/main/java/com/yau/doubao_community/controller/UmsUserController.java
@@ -60,4 +60,9 @@ public class UmsUserController extends BaseController{
         UmsUser user = iUmsUserService.getUserByUsername(userName);
         return ApiResult.success(user);
     }
+
+    @GetMapping("/logout")
+    public ApiResult<Object> logOut() {
+        return ApiResult.success("退出登录成功");
+    }
 }
